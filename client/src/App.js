8c33bd7f987f8a6e5fce106b0fd1696d74a13f3d
Aryan kaushik <Aryankaushik541@gmail.com>
2026-02-28 13:37:56 +0530

    Update App.js with authentication routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import ReviewPage from './pages/ReviewPage';
 import Dashboard from './pages/Dashboard';
 import './App.css';
@@ -8,23 +10,22 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navbar">
-          <div className="nav-container">
-            <h1 className="nav-logo">Review System</h1>
-            <ul className="nav-menu">
-              <li className="nav-item">
-                <Link to="/" className="nav-link">Reviews</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/dashboard" className="nav-link">Admin Dashboard</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
         <Routes>
-          <Route path="/" element={<ReviewPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          {/* Default route redirects to login */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          
+          {/* Authentication routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          
+          {/* User routes */}
+          <Route path="/reviews" element={<ReviewPage />} />
+          
+          {/* Admin routes */}
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          
+          {/* Catch all - redirect to login */}
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </div>
     </Router>
