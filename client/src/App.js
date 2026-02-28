973f1381fe11c36d34a7438ceb89086d4dd36912
Aryan kaushik <Aryankaushik541@gmail.com>
2026-02-28 16:47:34 +0530

    Update App.js - Set ReviewPage as default landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
 import ReviewPage from './pages/ReviewPage';
 import Dashboard from './pages/Dashboard';
-import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          {/* Default route redirects to login */}
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          
-          {/* Authentication routes */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          
-          {/* User routes */}
-          <Route path="/reviews" element={<ReviewPage />} />
-          
-          {/* Admin routes */}
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          
-          {/* Catch all - redirect to login */}
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </div>
+      <Routes>
+        {/* Default landing page - Public Review Page */}
+        <Route path="/" element={<ReviewPage />} />
+        <Route path="/reviews" element={<ReviewPage />} />
+        
+        {/* Authentication routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        
+        {/* Admin dashboard */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        
+        {/* Redirect any unknown routes to review page */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </Router>
   );
 }
